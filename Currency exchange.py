909040ad86168ab303b0c3c395327e4d00a2df55
Oyetosiayo <103373293+Oyetosiayo@users.noreply.github.com>
2022-04-13 15:53:35 -0700

    first code

--- Currency exchange.py
+++ Currency exchange.py
@@ -1,5 +1,5 @@
 
 print (int(input("Enter Naira rate per Dollar:")))
 print(int(input("Enter intDollar number:")))
-intNaira= int("Naira rate per Dollar")*(int("intDollar_number"))
+intNaira= int("Naira rate per Dollar")*(int("intDollar_number")))
 print(intNaira)
